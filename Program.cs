afd057c57dd92162e63b4864d4ed64f4ca9c8f01
Bruno Henri Garcia <brunohenri.g@live.com>
2022-06-28 22:27:13 -0300

    Initial

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace ValueTasksBenchmark
+{
+
+    [MemoryDiagnoser]
+    public class ValueTaskBenchamrk
+    {
+        readonly Repository _repo = new Repository();
+        readonly Random _rnd = new Random(42);
+        readonly string _data;
+
+
+        public ValueTaskBenchamrk()
+        {
+            _data = _rnd.Next().ToString();
+        }
+
+        [Benchmark]
+        public async Task RunTaskRandomAsync() => await _repo.GetDataWithTaskAsync(_data);
+
+
+        [Benchmark]
+        public async Task RunValueTaskRandomAsync() => await _repo.GetDataWithValueTaskAsync(_data);
+
+        [Benchmark]
+        public async Task RunTaskAsync() => await _repo.GetDataWithTaskAsync("CONST_KEY");
+
+
+        [Benchmark]
+        public async Task RunValueTaskAsync() => await _repo.GetDataWithValueTaskAsync("CONST_KEY");
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run(typeof(Program).Assembly);
+        }
+    }
+}
---
+++ Repository.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+
+namespace ValueTasksBenchmark
+{
+    public class Repository
+    {
+        IMemoryCache _taskCache = new MemoryCache(new MemoryCacheOptions());
+        IMemoryCache _valueCache = new MemoryCache(new MemoryCacheOptions());
+
+        public async Task<string> GetDataWithTaskAsync(string cacheKey)
+        {
+            var value = _taskCache.Get<string>(cacheKey);
+            if (value is null)
+            {
+                value = await GetData();
+                _taskCache.Set(cacheKey, value, TimeSpan.FromHours(1));
+            }
+            return value;
+        }
+
+        public async ValueTask<string> GetDataWithValueTaskAsync(string cacheKey)
+        {
+            var value = _valueCache.Get<string>(cacheKey);
+            if (value is null)
+            {
+                value = await GetData();
+                _valueCache.Set(cacheKey, value, TimeSpan.FromHours(1));
+            }
+            return value;
+        }
+
+        private async Task<string> GetData()
+        {
+            await Task.Delay(500);
+            return "data";
+        }
+    }
+}
